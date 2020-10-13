4254c6f03b38db73c69452d22ebfa9c7240e076f
Ying Da Wang <yingda.wang@unsw.edu.au>
2020-10-13 15:26:02 +1100

    mpigpulocal?

--- tests/lbpm_permeability_simulator.cpp
+++ tests/lbpm_permeability_simulator.cpp
@@ -25,7 +25,9 @@ int main(int argc, char **argv)
 	// Initialize MPI
 	int rank,nprocs;
 	MPI_Init(&argc,&argv);
-	MPI_Comm comm = MPI_COMM_WORLD;
+
+	MPI_Comm comm;
+	MPI_Comm_split_type(MPI_COMM_WORLD,MPI_COMM_TYPE_SHARED,rank,MPI_INFO_NULL,&comm);
 	MPI_Comm_rank(comm,&rank);
 	MPI_Comm_size(comm,&nprocs);
 	{
